15e898bc47235bf9b17d18d483ad1249c063e89b
Yong Sheng <yongsheng.telebort@gmail.com>
2023-11-05 18:00:58 +0800

    add hello

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
 // import * as Font from 'expo-Font';
 // import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 // import { AppLoading } from 'expo';
 // import Home from './routes/screens/home';
-import MainContainer from './routes/MainContainer';
+// Hello Adibah
+import MainContainer from "./routes/MainContainer";
 
 function App() {
-
-  return (
-    <MainContainer />
-  );
+  return <MainContainer />;
 }
 
 export default App;
